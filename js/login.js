2741d01df55b477af556d34e61c296ed70c9b4cf
Daniel Cabana <danielcabana0727@gmail.com>
2024-03-08 08:16:51 -0500

    class v1

--- js/login.js
+++ js/login.js
@@ -1,44 +1,53 @@
-const nombrseUsuario = document.getElementById('#nombres');
-const apellidosUsuario = document.getElementById('#apellidos');
-const emailUsuario = document.getElementById('#email');
-const passwordUsuario = document.getElementById('#password');
+const nombrseUser = document.getElementById('nombres');//-->Capturamos los datos del html
+const lastNameUser = document.getElementById('apellidos');
+const emailUser = document.getElementById('email');
+const passwordUser = document.getElementById('password');
+
 const btnLogin = document.getElementById('btnLogin');
 const btnLogout = document.getElementById('btnLogOut');
 
+const users = [];//-->Creamos la lista de los usuarios
+
+let valueUserName;//-->Creamos las variables donde estarán los datos capturados
+let valueUserLastName;
+let valueUseremail;
+let valueUserpassword;
+
+//Creamos un objeto User 
 class User{
     constructor({
-        nombres,
-        apellidos,
-        email,
-        password,
+        name = undefined,
+        lastName = undefined,
+        email = undefined,
+        password = undefined,
     }){
-        this.nombres = nombres;
-        this.apellidos = apellidos;
+        this.name = name;
+        this.lastName = lastName;
         this.email = email;
-        this.password = password; 
+        this.password = password;
     }
-}
+} 
 
-let register = () => {
-    usuario = new User({
-        nombres: nombrseUsuario.value,
-        apellidos: apellidosUsuario.value,
-        email: emailUsuario.value,
-        password: passwordUsuario.value,
-    });
+//Asignamos los valores de cada dato a sus variables
+nombrseUser.addEventListener('input', ()=>{valueUserName = nombrseUser.value});
+lastNameUser.addEventListener('input', ()=> {valueUserLastName = lastNameUser.value});
+emailUser.addEventListener('input', ()=> {valueUseremail = emailUser.value});
+passwordUser.addEventListener('input', ()=> {valueUserpassword = passwordUser.value});
 
-    const {nombres, apellidos, email, password} = usuario;
+//Informamos la instancia de un usuario y creamos la instancia de ese objeto
+btnLogin.addEventListener('click', () => {
+    alert(`Hellow ${valueUserName} ${valueUserLastName}, your email is ${valueUseremail} and your password is ${valueUserpassword}`);
+    users.push(new User({
+        name: valueUserName,
+        lastName: valueUserLastName,
+        email: valueUseremail,
+        password: valueUserpassword,
+    }));
+    console.table(users);
+});
 
-    console.log(nombres);
-    console.log(apellidos);
-    console.log(email);
-    console.log(password);
-    alert('Registro exitoso');
-};
-
-let noRegister = () => {
+btnLogout.addEventListener('click', () => {
     alert('Registro desechado');
-}
+});
 
-btnLogin.addEventListener('click', register);
-btnLogout.addEventListener('click', noRegister);
+
